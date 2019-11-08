b2f48dc2c8276212bb469e527af88de0275a4142
Yan Pan <yp519@nyu.edu>
2019-11-07 21:33:00 -0500

    nov 7th progress

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends React.Component {
+  state= {
+    todos: [
+      {
+        id:1,
+        title: 'pick up grocery',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'eat an apple',
+        completed: true
+      }
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
-function Todos() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+class Todos extends React.Component {
+
+    render(){
+        console.log(this.props.todos);
+        return this.props.todos.map( todo =>(
+            <TodoItem key={todo.id} todo={todo} />
+        ));
+    }
+}
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
 }
 
 export default Todos;
